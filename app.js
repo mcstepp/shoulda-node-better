cee0a9933be263f4a734c48c33fcac11f07a5382
Meg Stepp <mcstepp@users.noreply.github.com>
2017-09-19 23:09:47 -0400

    hello world 1.1

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello world!');
+console.log('Starting app');
+
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+
+// DeprecationWarning: Calling an asynchronous function without callback is deprecated.
+// fs.appendFile('greetings.txt', 'Hello world!');
+
+// option 1
+fs.appendFile('greetings.txt', 'Hello ' + user.username + '!', function (err){
+    if (err) {
+        console.log('Unable to write to file');
+    }
+});
+
+// option 2
+// fs.appendFileSync('greetings.txt', 'Hello World!');

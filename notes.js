40442ef7fadb6749b4d90f116f47360bac613106
Meg Stepp <mcstepp@users.noreply.github.com>
2017-10-16 23:05:40 -0400

    add note

--- notes.js
+++ notes.js
@@ -1,7 +1,33 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
+
 var addNote = (title, body) => {
-    console.log('Adding note', title, body);
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (error) {
+        // nothing to catch
+    }
+
+    var duplicateNotes = notes.filter((note) => {
+        // returns true or false
+        //if true if match, false if not
+        return note.title === title;
+    });
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 
 var getAll = () => {
